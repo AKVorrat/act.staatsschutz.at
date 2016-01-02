44dca873163e1369a0486adcc4a522ad4050ae47
PublicScam <publicscam@gmx.at>
2016-01-03 00:44:35 +0100

    populate modals

--- js/slider.js
+++ js/slider.js
@@ -1,11 +1,13 @@
 var searchParties = {"spoe": true, "oevp": true, "fpoe": false, "gruene": false, "neos": false, "frank": false, "none": false};
 var searchTeams = {"liberty": false, "spy": true, "unknown": false};
-var representatives, format, parties;
+var representatives, format, parties, teams, genders;
 var blocked = true;
 var filteredRepresentatives;
 var jsonRepresentatives = "./data/representatives.json";
 var jsonFormat = "./data/format.json";
 var jsonParties = "./data/parties.json";
+var jsonTeams = "./data/teams.json";
+var jsonGenders = "./data/genders.json";
 var imgPath = "./img/representatives/";
 var packageSize, index = 0;
 
@@ -30,6 +32,17 @@ function calculatePackageSize() {
         packageSize = rowSize;
 }
 
+function jsonResolve(target, representative) {
+    var str = target;
+    for (var sub in representative) {
+        str = str.replace(new RegExp("%" + sub.toUpperCase() + "%", 'g'), representative[sub]);
+    }
+    for (var sub in genders) {
+        str = str.replace(new RegExp("%" + sub + "%", 'g'), genders[sub][representative.gender]);
+    }
+    return str;
+}
+
 function setListeners() {
     $("#partyInput :checkbox").change(function () {
         searchParties[$(this).attr("id")] = !searchParties[$(this).attr("id")];
@@ -48,9 +61,26 @@ function setListeners() {
         var representative = button.data("representative");
         representative = representatives[representative];
         var modal = $(this);
-        var name = representative.title + " " + representative.firstname + " " + representative.lastname;
-        modal.find(".modal-title").text("Kontaktiere " + name);
-        modal.find(".modal-body").text("Kann " + name + " unsere Bürgerrechte bewahren?");
+        var $modalTitle = modal.find(".modal-title");
+        var $modalContent = modal.find(".modal-body");
+        
+        var name = representative.firstname + " " + representative.lastname;
+        if (representative.title != "")
+            name = representative.title + " " + name;
+        
+        $modalTitle.text(name);
+        
+        inArray = representatives.indexOf(representative);
+        img = imgPath + inArray + ".jpg";
+        $.get(img).fail(function () {
+            img = imgPath + "none.gif";
+        })
+        
+        $modalContent.html($("<div class='row' style='position: relative;'></div>")
+            .append($("<img class='img-responsive center-block' src='" + img + "' />"))
+            .append($("<div class='teamSign " + representative.team + "' style='background: " + teams[representative.team].color + ";'>" + teams[representative.team].name + "</div>")));
+        $modalContent.append($("<p></p>").append(jsonResolve(teams[representative.team].introduction, representative) + " ")
+            .append($("<span style='color: " + teams[representative.team].color + ";'></span>").text(jsonResolve(teams[representative.team].todo, representative))));
     })
 }
 
@@ -67,6 +97,12 @@ $(document).ready(function () {
     });
     requestJSON(jsonParties, function (passParties) {
         parties = passParties.entries;
+    });
+    requestJSON(jsonTeams, function (passTeams) {
+        teams = passTeams.entries;
+    });
+    requestJSON(jsonGenders, function (passGenders) {
+        genders = passGenders.entries;
     });
     requestJSON(jsonRepresentatives, function (passRepresentatives) {
         representatives = passRepresentatives.entries;
